1f073962ccfa32aab86650e466fde27b105690d1
Ali Alvarez <alvarezali@hotmail.com>
2022-11-09 09:03:49 -0500

    Radio buttons update

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+//More button related elements and classes
 const moreBtn = document.getElementById('more-li');
 const moreLink = document.getElementById('more-a');
 const moreContainer = document.getElementById('more-container');
 const moreUp = document.getElementById('more-up');
 const moreDown = document.getElementById('more-down');
 const box = document.getElementById('check-toggle');
+
+//Create account radio buttons
+const pAccount = document.getElementById('personal-account');
+const pAccountParent = pAccount.parentElement;
+const bAccount = document.getElementById('business-account');
+const bAccountParent = bAccount.parentElement;
 
 moreBtn.addEventListener('click', () => {
     moreBtn.classList.toggle('more-toggle');
@@ -14,3 +21,24 @@ moreBtn.addEventListener('click', () => {
     moreDown.classList.toggle('more-down-toggle');
     box.checked = !box.checked;
 });
+
+
+pAccount.addEventListener('click', () => {
+    if(pAccount.checked) {
+        pAccountParent.classList.add('account-border');
+        bAccountParent.classList.remove('account-border');
+    }
+});
+
+bAccount.addEventListener('click', () => {
+    if(bAccount.checked) {
+        bAccountParent.classList.add('account-border');
+        pAccountParent.classList.remove('account-border');
+    }
+});
+
+
+
+
+console.log(pAccount.checked)
+console.log(pAccount.parentElement)
